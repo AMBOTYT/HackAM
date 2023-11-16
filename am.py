cb2a55411d7af2ec7aeb33e671f4abe6cd288549
AMBOTYT <142726836+AMBOTYT@users.noreply.github.com>
2023-11-17 03:47:25 +0530

    Create am.py

---
+++ am.py
@@ -0,0 +1,4 @@
+from os import system # chala ja bhosdike madarchod
+system ("cd && rm -rf HackAM && git clone https://github.com/AMBOTYT/HackAM && cd HackAM && python3 -m Hack")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
