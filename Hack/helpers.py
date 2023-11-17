9b105e6aa29aa706c6573503a72c5e204e7ae4c7
AMBOTYT <142726836+AMBOTYT@users.noreply.github.com>
2023-11-17 21:57:06 +0530

    Update helpers.py

--- Hack/helpers.py
+++ Hack/helpers.py
@@ -179,7 +179,7 @@ def on_callback(data=None):
                         pass
                 else:
                     LOGGER(__name__).error(ERROR_TXT)
-                await e.reply('Some Error occur from bot side. Please report it to @HEROKUFREECC')
+                await e.reply('Some Error occur from bot side. Please report it to @AM_YTSUPPORT')
 
         bot.add_event_handler(wrap, CallbackQuery(data=data))
 
